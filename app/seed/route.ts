ebf23fb9d77fc795eececf465034e70a6b26dcf9
Andreas Kevin <andreaskevin819@gmail.com>
2024-10-29 08:27:46 +0700

    jssnove

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -285,10 +285,10 @@ async function seedRoles() {
 }
 
 export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
+  // return Response.json({
+  //   message:
+  //     'Uncomment this file and remove this line. You can delete this file when you are finished.',
+  // });
   try {
     await client.sql`BEGIN`;
     await seedUsers();
